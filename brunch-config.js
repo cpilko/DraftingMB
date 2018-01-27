75d2ae109552bca25e845c20bf24ad642e9d32f8
Chris Pilko <chris.pilko@gmail.com>
2018-01-26 20:12:46 -0500

    first commit

---
+++ brunch-config.js
@@ -0,0 +1,63 @@
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'vendor.js': /^node_modules/,
+      'app.js': /^app/
+    },
+  },
+  stylesheets: {
+    joinTo: {
+      'vendor.css': /^node_modules/,
+      'app.css': /^app/
+    }
+  }
+};
+
+exports.paths = {
+    public: 'docs'
+};
+
+exports.plugins = {
+  babel: { presets: ['latest'] },
+  autoReload: {
+    enabled: {
+      css: true,
+      js: true,
+      assets: true
+    }
+  },
+  pug: {
+    basedir: 'app/layouts',
+    staticBasedir: 'app/layouts',
+    pugRuntime: false,
+    preCompile: true,
+    preCompilePattern: /\.pug$/
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  exports.npm = {
+    styles: {
+      'semantic-ui-css': ['semantic.min.css'],
+      'basscss': ['css/basscss.min.css']
+    },
+  }
+} else {
+  exports.npm = {
+    styles: {
+      'semantic-ui-css': ['semantic.css'],
+      'basscss': ['css/basscss.css']
+    },
+  }
+}
+
+exports.modules = {
+  nameCleaner: path => path.replace(/^app\//, ''),
+  autoRequire: {
+    'app.js': ['app']
+  }
+}
+
+exports.conventions = {
+  ignored: [/^app\/layouts/, /\.bak$/, /^_/]
+}
